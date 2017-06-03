69e60ef477ef59a1717aa1b66ae61393d65ea288
chris062689 <chris062689@gmail.com>
2017-06-02 21:36:17 -0400

    Restructed game content.

--- scripts/games/app.js
+++ scripts/games/app.js
@@ -1,36 +1,48 @@
-var fs = require('fs');
-var fsextra = require('fs-extra');
-var util = require('util');
-var logger = require('winston');
+const fs = require('fs');
+const fsextra = require('fs-extra');
+const path = require('path');
+const util = require('util');
+const logger = require('winston');
 
-var sanitizeHtml = require('sanitize-html');
+const sanitizeHtml = require('sanitize-html');
 
-var del = require('delete');
-var exec = require('sync-exec');
+const blackfriday = require('./blackfriday.js');
 
-var inputDirectoryGame = './citra-games-wiki/';
-var inputDirectoryWiki = './citra-games-wiki.wiki/';
-var outputDirectoryMd = '../../site/content/game/';
-var outputDirectoryBoxart = '../../site/static/images/games/'
+const del = require('delete');
+const exec = require('sync-exec');
+
+const inputDirectoryGame = './citra-games-wiki';
+const inputDirectoryWiki = './citra-games-wiki.wiki';
+const outputDirectoryMd = '../../site/content/game';
+const outputDirectoryBoxart = '../../site/static/images/games/boxart';
 
 // The URL
 function url(title) {
   return '/wiki/' + title.replace(/\s+/g, '-').toLowerCase();
 }
 
-if (fs.existsSync(inputDirectoryGame)) {
-    logger.info(`Purging input directory game: ${inputDirectoryGame}`);
-    del.sync(inputDirectoryGame, {force: true});
+function gitPull(directory, repository) {
+  if (fs.existsSync(directory)) {
+      logger.info(`Fetching latest from Github : ${directory}`);
+      exec(`git --git-dir=${directory} pull`);
+  } else {
+      logger.info(`Cloning repository from Github : ${directory}`);
+      exec(`git clone ${repository}`);
+  }
 }
 
-if (fs.existsSync(inputDirectoryWiki)) {
-    logger.info(`Purging input directory wiki: ${inputDirectoryWiki}`);
-    del.sync(inputDirectoryWiki, {force: true});
+function getDirectories (srcpath) {
+  return fs.readdirSync(srcpath)
+    .filter(file => fs.lstatSync(path.join(srcpath, file)).isDirectory())
 }
 
-exec('git clone https://github.com/citra-emu/citra-games-wiki.git');
-exec('git clone https://github.com/citra-emu/citra-games-wiki.wiki.git');
+// Fetch game information stored in Github repository.
+// gitPull(inputDirectoryGame, 'https://github.com/citra-emu/citra-games-wiki.git');
 
+// Fetch game articles stored in Github wiki.
+// gitPull(inputDirectoryWiki, 'https://github.com/citra-emu/citra-games-wiki.wiki.git');
+
+// Make sure the output directories in Hugo exist.
 if (fs.existsSync(outputDirectoryMd) == false) {
     logger.info(`Creating missing output directory: ${outputDirectoryMd}`);
     fs.mkdirSync(outputDirectoryMd);
@@ -41,71 +53,31 @@ if (fs.existsSync(outputDirectoryBoxart) == false) {
     fs.mkdirSync(outputDirectoryBoxart);
 }
 
-fs.readdir(inputDirectoryGame, function(err, items) {
-    try {
-      // Look for all .md files within the wiki directory.
-      items.filter(file => file.substr(-4) === '.png').forEach(function(item) {
-        logger.info(`Copying boxart PNG ${item}`);
-        fsextra.copySync(`${inputDirectoryGame}${item}`, `${outputDirectoryBoxart}${item}`);
-      });
-      items.filter(file => file.substr(-4) === '.dat').forEach(function(item) {
-        // Generate the title from the filename.
-        let title = item.replace(/-/g, ' ').slice(0, -3);
-        var stats = fs.statSync(`${inputDirectoryGame}${item}`);
-        var modified = new Date(util.inspect(stats.mtime));
+try {
+  // Loop through each game folder.
+  getDirectories(inputDirectoryGame).forEach(function(game) {
+    if (game == '.git') { return; }
 
-        // Read the .dat file and the .md file and fuse them.
-        fs.readFile(`${inputDirectoryGame}${item}`, 'utf8', function (err,data) {
-          if (err) { logger.error(err); return; }
+    logger.info(`Creating Hugo files for ${game}`);
 
-          try {
-            // Convert various data inside of the markdown language.
-            let cleanGameData = data;
-            let cleanWikiData = fs.readFileSync(`${inputDirectoryWiki}${item.replace('.dat', '.md')}`, 'utf8');
+    // Copy the boxart for the game.
+    fsextra.copySync(`${inputDirectoryGame}/${game}/boxart.png`, `${outputDirectoryBoxart}/${game}.png`);
 
-            // Blackfriday Markdown Rendering requires a blank line before lists.
-            try {
-              var lines = cleanWikiData.split(/\r?\n/);
-              for(var i = 0; i < lines.length; i++) {
-                  // If it's the start of the file, ignore to prevent an index issue.
-                  if (i > lines.length) { return; }
-                  if (i == 0 || lines[i] == '\n') { continue; }
+    // Create the markdown file to be displayed in Hugo.
+    let title = game.replace(/-/g, ' ').slice(0, -3);
+    var stats = fs.statSync(`${inputDirectoryGame}/${game}/game.dat`);
+    var modified = new Date(util.inspect(stats.mtime));
 
-                  // Search for the start of a list designated by the * character.
-                  if (lines[i].startsWith("* ") && lines[i - 1].startsWith("* ") == false) {
-                    i = i + 1;
-                    lines.splice(i - 1, 0, '');
-                  }
-              }
-              cleanData = lines.join('\n');
-            } catch (ex) {
-              logger.error(ex);
-            }
+    let datContents = fs.readFileSync(`${inputDirectoryGame}/${game}/game.dat`, 'utf8');
+    let wikiContents = fs.readFileSync(`${inputDirectoryWiki}/${game}.md`, 'utf8');
 
-            // Replacing tags like [[Common Issues on Windows|Common Issues]]
-            cleanWikiData = cleanWikiData.replace(/\[\[(.*)\|(.*)\]\]/g, function(match, p1, p2) {
-              return `[${p1}](${url(p2)})`
-            });
+    // Fix Blackfriday markdown rendering differences.
+    wikiContents = blackfriday.fixLists(wikiContents);
+    wikiContents = blackfriday.fixLinks(wikiContents);
 
-            // Replacing tags like [[Common Issues]]
-            cleanWikiData = cleanWikiData.replace(/\[\[(.*)\]\]/g, function(match, p1) {
-              return `[${p1}](${url(p1)})`
-            });
-
-            // Create the new markdown header for Hugo.
-            let newFileContents = `+++\r\ndate = "${modified.toISOString()}"\r\n${cleanGameData}+++\r\n\r\n${cleanWikiData}\r\n`;
-
-            let itemOutput = item.toLowerCase().replace('.dat', '.md');
-            fs.writeFile(`${outputDirectoryMd}${itemOutput}`, newFileContents, function(err) {
-                if (err) return logger.error(err);
-                logger.info(`Wrote file ${itemOutput} to filesystem.`);
-            });
-          } catch (ex) {
-            logger.error(ex);
-          }
-        });
-      });
-    } catch (ex) {
-      logger.error(ex);
-    }
-});
+    let output = `+++\r\ndate = "${modified.toISOString()}"\r\n${datContents}+++\r\n\r\n${wikiContents}\r\n`;
+    fs.writeFileSync(`${outputDirectoryMd}/${game}.md`, output);
+  });
+} catch (ex) {
+  logger.error(ex);
+}
