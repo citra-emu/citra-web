2c6d226a2253341f4ec225ee793a9020141da170
chris062689 <chris062689@gmail.com>
2017-06-24 19:04:10 -0400

    Updated gulp file to be able to run hugo locally.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 const gulp = require('gulp');
+const util = require('gulp-util');
 const exec = require('child_process').exec;
 const rimraf = require('rimraf');
 
+const browsersync = require('browser-sync');
 const ghPages = require('gulp-gh-pages');
 
 const md5 = require("gulp-md5-plus");
@@ -22,14 +24,26 @@ const deployOptions = {
   branch: "master"
 };
 
-gulp.task("default", ['html']);
+gulp.task("default", ['serve']);
 
+// PHASE 1 - Setup
 gulp.task('setup', function(cb) {
+  if (util.env.production) {
     process.env.HUGO_ENV = 'PRD';
-    process.env.GULP = 'true';
-    rimraf(`${distPath}`, cb);
+  } else {
+    process.env.HUGO_ENV = 'DEV';
+  }
+  process.env.GULP = 'true';
+
+  util.log(`process.env.HUGO_ENV = '${process.env.HUGO_ENV}'`);
+  util.log(`process.env.GULP = '${process.env.GULP}'`);
+
+  rimraf(`${distPath}`, cb);
 });
 
+// PHASE 2 - Data Dependencies
+
+// PHASE 3 - Building
 gulp.task('hugo', ['setup'], function (cb) {
   exec('hugo -s ./site/ -v', function (err, stdout, stderr) {
     console.log(stdout);
@@ -41,14 +55,6 @@ gulp.task('hugo', ['setup'], function (cb) {
 gulp.task("css", ['hugo'], () => (
   gulp.src(`${distPath}/css/**/*.css`, {base: './'})
     .pipe(postcss([cssnext(), cssImport({from: `${distPath}/css/main.css`})]))
-    .pipe(cleanCSS())
-    .pipe(md5(10, `${distPath}/**/*.html`))
-    .pipe(gulp.dest('./'))
-));
-
-gulp.task("js", ['hugo'], () => (
-  gulp.src(`${distPath}/js/**/*.js`, {base: './'})
-    .pipe(md5(10, `${distPath}/**/*.html`))
     .pipe(gulp.dest('./'))
 ));
 
@@ -67,19 +73,44 @@ gulp.task('images', ['hugo'], () => (
   gulp.src(`${distPath}/images/game/icons/*`, {base: './'})
       .pipe(imageResize({ width: 48, height: 48, crop: true }))
       .pipe(gulp.dest('./')),
-  
+
   gulp.src(`${distPath}/images/screenshots/*`)
       .pipe(imageResize({ width: 400, height: 240, crop: false }))
       .pipe(gulp.dest(`${distPath}/images/screenshots/thumbs`))
 ));
 
-gulp.task('html', ['hugo', 'css', 'js', 'images'], () => (
+// This task ensures all phases up to PHASE 3 are completed.
+gulp.task('build', ['hugo', 'css', 'images']);
+
+// STAGE 4 - Optimization
+gulp.task('compress', ['build'], () => (
+  gulp.src(`${distPath}/js/**/*.js`, {base: './'})
+    .pipe(md5(10, `${distPath}/**/*.html`))
+    .pipe(gulp.dest('./')),
   gulp.src(`${distPath}/**/*.html`, {base: './'})
     .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('./')),
+  gulp.src(`${distPath}/css/**/*.css`, {base: './'})
+    .pipe(cleanCSS())
+    .pipe(md5(10, `${distPath}/**/*.html`))
     .pipe(gulp.dest('./'))
 ));
 
-gulp.task('deploy', ['html'], () => {
+// STAGE 5 - Deploy
+
+// Used for Development
+gulp.task('serve', ['build'], () => {
+  // Serve files from the root of this project
+  browsersync({
+      server: {
+          baseDir: distPath
+      },
+      open: false
+  });
+});
+
+// Used for Production
+gulp.task('deploy', ['build', 'compress'], () => {
   require('fs').writeFileSync(`${distPath}/CNAME`, `${cname}`);
   require('fs').writeFileSync(`${distPath}/robots.txt`, `Sitemap: https://${cname}/sitemap.xml\n\nUser-agent: *`);
   return gulp.src(`${distPath}/**/*`).pipe(ghPages(deployOptions));
