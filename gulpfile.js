1b7209a4fa4e547baae58c8c48c6b5401b95c2aa
chris062689 <chris062689@gmail.com>
2017-08-14 23:13:06 -0400

    Initial checkin of refactoring gulp / scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,135 +1,128 @@
 const gulp = require('gulp');
-const util = require('gulp-util');
+const runSequence = require('run-sequence');
 const exec = require('child_process').exec;
-const rimraf = require('rimraf');
-
-const browsersync = require('browser-sync');
-const ghPages = require('gulp-gh-pages');
-
-const md5 = require("gulp-md5-plus");
-const postcss = require('gulp-postcss');
-const cssImport = require('postcss-import');
-const cssnext = require('postcss-cssnext');
+const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
+const browserSync = require('browser-sync').create();
 
-const imageResize = require('gulp-image-resize');
+const publishLocation = '/home/chris/www/';
+const finalCommand = 'final:serve';
 
-const htmlmin = require('gulp-htmlmin');
+// Gulp Run Tasks
 
-const hugoPath = './site';
-const distPath = './public';
-const cname = 'citra-emu.org';
-const deployOptions = {
-  remoteUrl: "git@github.com:CitraBotWeb/CitraBotWeb.github.io.git",
-  branch: "master"
-};
-
-gulp.task("default", ['setup', 'serve']);
-
-// PHASE 1 - Setup
-gulp.task('setup', function(cb) {
-  if (util.env.production) {
-    process.env.HUGO_ENV = 'PRD';
-  } else {
-    process.env.HUGO_ENV = 'DEV';
-  }
-  process.env.GULP = 'true';
-
-  util.log(`process.env.HUGO_ENV = '${process.env.HUGO_ENV}'`);
-  util.log(`process.env.GULP = '${process.env.GULP}'`);
-
-  rimraf(`${distPath}`, cb);
+gulp.task('default', function(callback) {
+  runSequence('hugo', finalCommand, callback);
 });
 
-// PHASE 2 - Building
-gulp.task('hugo', function (cb) {
-  exec('hugo -s ./site/ -d ../public/ -v', function (err, stdout, stderr) {
+gulp.task('all', function(callback) {
+  runSequence(['scripts:downloads', 'scripts:games', 'scripts:twitter', 'scripts:wiki'],
+              ['assets:js', 'assets:fonts', 'assets:css'],
+              'hugo',
+              finalCommand,
+              callback);
+});
+
+gulp.task('downloads', function(callback) {
+  runSequence('scripts:downloads', 'hugo', finalCommand, callback);
+});
+
+gulp.task('games', function(callback) {
+  runSequence('scripts:games', 'hugo', finalCommand, callback);
+});
+
+gulp.task('twitter', function(callback) {
+  runSequence('scripts:twitter', 'hugo', finalCommand, callback);
+});
+
+gulp.task('wiki', function(callback) {
+  runSequence('scripts:wiki', 'hugo', finalCommand, callback);
+});
+
+gulp.task('assets', function(callback) {
+  runSequence(['assets:js', 'assets:fonts', 'assets:css'], 'hugo', finalCommand, callback);
+});
+
+// Gulp Pipeline
+
+gulp.task('setup', function() {
+    process.env.HUGO_BASEURL = 'http://localhost:3000';
+    process.env.HUGO_ENV = 'DEV';
+});
+
+gulp.task('scripts:downloads', ['setup'], function (callback) {
+  exec(`cd ./scripts/downloads/ && npm install && node app.js`, function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    callback(err);
+  });
+});
+
+gulp.task('scripts:games', ['setup'], function (callback) {
+  exec(`cd ./scripts/games/ && npm install && node app.js`, function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    callback(err);
+  });
+});
+
+gulp.task('scripts:twitter', ['setup'], function (callback) {
+  exec(`cd ./scripts/twitter/ && npm install && node app.js`, function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    callback(err);
+  });
+});
+
+gulp.task('scripts:wiki', ['setup'], function (callback) {
+  exec(`cd ./scripts/wiki/ && npm install && node app.js`, function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    callback(err);
+  });
+});
+
+gulp.task('assets:js', function(){
+  return gulp.src('assets/css/styles.less')
+    .pipe(gulp.dest('build/js'))
+});
+
+gulp.task('assets:fonts', function(){
+  return gulp.src('./node_modules/bootstrap-sass/assets/fonts/*/**')
+    .pipe(gulp.dest('build/fonts/'))
+});
+
+gulp.task('assets:css', function () {
+  return gulp.src('assets/css/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('build/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('hugo', ['setup'], function (cb) {
+  exec('hugo -s ./site/ -d ../build/ -v', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     cb(err);
   });
 });
 
-gulp.task("css", ['hugo'], () => (
-  gulp.src(`${distPath}/css/**/*.css`, {base: './'})
-    .pipe(postcss([cssnext(), cssImport({from: `${distPath}/css/main.css`})]))
-    .pipe(gulp.dest('./'))
-));
-
-gulp.task('images', ['hugo'], () => (
-  gulp.src(`${distPath}/images/*`, {base: './'})
-      .pipe(gulp.dest('./')),
-  gulp.src(`${distPath}/images/jumbotron/*`, {base: './'})
-      .pipe(imageResize({ width: 786, height: 471, crop: true }))
-      .pipe(gulp.dest('./')),
-  gulp.src(`${distPath}/images/banners/*`, {base: './'})
-      .pipe(imageResize({ width: 824, height: 306, crop: false }))
-      .pipe(gulp.dest('./')),
-  gulp.src(`${distPath}/images/game/boxart/*`, {base: './'})
-      .pipe(imageResize({ width: 328, height: 300, crop: true }))
-      .pipe(gulp.dest('./')),
-  gulp.src(`${distPath}/images/game/icons/*`, {base: './'})
-      .pipe(imageResize({ width: 48, height: 48, crop: true }))
-      .pipe(gulp.dest('./')),
-
-  gulp.src(`${distPath}/images/screenshots/*`)
-      .pipe(imageResize({ width: 400, height: 240, crop: false }))
-      .pipe(gulp.dest(`${distPath}/images/screenshots/thumbs`))
-));
-
-gulp.task('repogen', ['hugo'], function (cb) {
-    exec('node app.js', {
-        cwd: "scripts/downloads"
-    }, function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        cb(err);
+gulp.task('final:serve', ['setup'], function() {
+    browserSync.init({
+        open: false,
+        server: {
+            baseDir: 'build'
+        }
     });
+
+    gulp.watch('assets/js/**/*', ['assets:js']);
+    gulp.watch('assets/css/**/*', ['assets:css']);
+    gulp.watch('site/**/*.html', ['deploy-hugo']);
+
+    gulp.watch('build/**/*').on('change', browserSync.reload);
 });
 
-gulp.task('repo', ['repogen'], () => (
-    gulp.src(`scripts/downloads/build/**`)
-        .pipe(gulp.dest(`${distPath}/repository`))
-));
-
-// This task ensures all phases up to PHASE 2 are completed.
-gulp.task('build', ['hugo', 'css', 'images', 'repo'], function (done) {
-    browsersync.reload();
-    done();
-});
-
-// STAGE 3 - Optimization
-gulp.task('compress', ['build'], () => (
-  gulp.src(`${distPath}/js/**/*.js`, {base: './'})
-    .pipe(md5(10, `${distPath}/**/*.html`))
-    .pipe(gulp.dest('./')),
-  gulp.src(`${distPath}/**/*.html`, {base: './'})
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('./')),
-  gulp.src(`${distPath}/css/**/*.css`, {base: './'})
-    .pipe(cleanCSS())
-    .pipe(md5(10, `${distPath}/**/*.html`))
-    .pipe(gulp.dest('./'))
-));
-
-// STAGE 4 - Deploy
-
-// Used for Development
-gulp.task('serve', ['build'], () => {
-  // Serve files from the root of this project
-  browsersync({
-      server: {
-          baseDir: distPath
-      },
-      open: false
-  });
-
-  gulp.watch('./site/**/*', ['build']);
-});
-
-// Used for Production
-gulp.task('deploy', ['setup', 'build', 'compress'], () => {
-  require('fs').writeFileSync(`${distPath}/CNAME`, `${cname}`);
-  require('fs').writeFileSync(`${distPath}/robots.txt`, `Sitemap: https://${cname}/sitemap.xml\n\nUser-agent: *`);
-  return gulp.src(`${distPath}/**/*`).pipe(ghPages(deployOptions));
+gulp.task('final:publish', ['setup'], function(){
+  return gulp.src('build')
+    .pipe(gulp.dest(publishLocation))
 });
